3f0d90afaa046de89f305e2286eb6cc6696e77e8
= <shakirck333@gmail.com>
2020-09-29 15:41:15 +0530

    Updates

---
+++ precache-manifest.64c83081c1b9158e1c7686852299e5df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ecf4a35e2c3d012e43baf1550b72b3",
+    "url": "/College-List-App-/index.html"
+  },
+  {
+    "revision": "c98b52e4fa955c34eb35",
+    "url": "/College-List-App-/static/css/main.605c788d.chunk.css"
+  },
+  {
+    "revision": "2c81562c4365c79383c0",
+    "url": "/College-List-App-/static/js/2.69acc574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/College-List-App-/static/js/2.69acc574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98b52e4fa955c34eb35",
+    "url": "/College-List-App-/static/js/main.e4300cd6.chunk.js"
+  },
+  {
+    "revision": "8beaee80511d4e16612c",
+    "url": "/College-List-App-/static/js/runtime-main.669401a8.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/College-List-App-/static/media/college_02.619ee97e.jpg"
+  },
+  {
+    "revision": "08930f6418fbe961bcd2260a33d948a0",
+    "url": "/College-List-App-/static/media/loading.08930f64.gif"
+  }
+]);
